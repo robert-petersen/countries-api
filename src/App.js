fbbed90f212076aa0bb0e578475548c6283437dd
Robert Petersen <robert.petersen808@gmail.com>
2021-10-08 22:28:37 -0500

    created basic file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Home from "./views/Home";
+import Details from "./views/Details";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <NavBar />
+      <Switch>
 
-    </div>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/details" exact>
+          <Details />
+        </Route>
+        
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
