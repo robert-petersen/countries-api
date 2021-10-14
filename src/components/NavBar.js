e808e8aa04841e5f259adb4328ddd8b17cb40b9f
Robert Petersen <robert.petersen808@gmail.com>
2021-10-13 21:06:46 -0500

    Nav styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,12 @@ const NavBar = ({ theme, setTheme }) => {
 
   return (
     <nav>
-      <div>
-        <h1>Where in the World?</h1>
+      <div className="nav_title_container" >
+        <h1 className="nav_title" >Where in the World?</h1>
       </div>
-      <div onClick={changeTheme} >
-        { theme ? <BsMoon /> : <BsFillMoonFill /> }
-        <h2>{ theme ? "Light Theme" : "Dark Theme" }</h2>
+      <div onClick={changeTheme} className="nav_theme_container" >
+        { theme ? <BsMoon className="nav_theme_icon" /> : <BsFillMoonFill className="nav_theme_icon" /> }
+        <h2 className="nav_theme" >{ theme ? "Light Theme" : "Dark Theme" }</h2>
       </div>
     </nav>
   )
